a962901b0e46a8c7b83c52004a9d2b1f7ba9ffce
FuyukiVila <1642421711@qq.com>
2025-10-28 16:33:02 +0800

    feat: complete ByteStream

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -6,50 +6,61 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
 void Writer::push( string data )
 {
-  (void)data; // Your code here.
+  uint64_t bytes_to_write = min( static_cast<uint64_t>( data.size() ), available_capacity() );
+  for ( uint64_t i = 0; i < bytes_to_write; ++i ) {
+    buffer_.push_back( data[i] );
+  }
+  push_count_ += bytes_to_write;
 }
 
 void Writer::close()
 {
-  // Your code here.
+  is_closed_ = true;
 }
 
 bool Writer::is_closed() const
 {
-  return {}; // Your code here.
+  return is_closed_;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  return {}; // Your code here.
+  return capacity_ - buffer_.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  return {}; // Your code here.
+  return push_count_;
 }
 
 string_view Reader::peek() const
 {
-  return {}; // Your code here.
+  if ( buffer_.empty() ) {
+    return {};
+  }
+  return string_view( &buffer_.front(), 1 );
 }
 
 void Reader::pop( uint64_t len )
 {
-  (void)len; // Your code here.
+  uint64_t bytes_to_pop = min( len, static_cast<uint64_t>( buffer_.size() ) );
+  for ( uint64_t i = 0; i < bytes_to_pop; ++i ) {
+    buffer_.pop_front();
+  }
+  pop_count_ += bytes_to_pop;
 }
 
 bool Reader::is_finished() const
 {
-  return {}; // Your code here.
+  return is_closed_ && buffer_.empty();
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  return {}; // Your code here.
+  return buffer_.size();
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  return {}; // Your code here.
+  return pop_count_;
 }
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "deque"
 #include <cstdint>
 #include <string>
 #include <string_view>
@@ -25,6 +26,10 @@ protected:
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
   uint64_t capacity_;
   bool error_ {};
+  std::deque<char> buffer_ {};
+  uint64_t push_count_ {};
+  uint64_t pop_count_ {};
+  bool is_closed_ {};
 };
 
 class Writer : public ByteStream
